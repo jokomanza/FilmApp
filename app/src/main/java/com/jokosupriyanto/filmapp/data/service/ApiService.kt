3d9c17f9ab3b35d1658e809ad368820832df5bca
Joko Supriyanto <jokomanza@gmail.com>
2021-06-03 15:22:10 +0700

    update api service

--- app/src/main/java/com/jokosupriyanto/filmapp/data/service/ApiService.kt
+++ app/src/main/java/com/jokosupriyanto/filmapp/data/service/ApiService.kt
@@ -1,4 +1,20 @@
 package com.jokosupriyanto.filmapp.data.service
 
-class ApiService {
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiService {
+
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("http://api.tvmaze.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun<T> buildService(service: Class<T>): T{
+        return retrofit.create(service)
+    }
 }

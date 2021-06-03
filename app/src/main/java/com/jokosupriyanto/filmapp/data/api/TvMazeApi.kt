5866d0dfbf70ab2cd55f8821051f49925bc96146
Joko Supriyanto <jokomanza@gmail.com>
2021-06-03 15:22:58 +0700

    update api interface

--- app/src/main/java/com/jokosupriyanto/filmapp/data/api/TvMazeApi.kt
+++ app/src/main/java/com/jokosupriyanto/filmapp/data/api/TvMazeApi.kt
@@ -1,4 +1,12 @@
 package com.jokosupriyanto.filmapp.data.api
 
+import com.jokosupriyanto.filmapp.data.model.Result
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface TvMazeApi {
+
+    @GET("search/shows")
+    fun findBy(@Query("q") key: String): Call<Result>
 }

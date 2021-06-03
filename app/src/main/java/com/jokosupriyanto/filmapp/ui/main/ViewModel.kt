9cbf974e11ccec01ffcefcf5175f5d7b89ae183e
Joko Supriyanto <jokomanza@gmail.com>
2021-06-03 15:21:50 +0700

    create sample view model

---
+++ app/src/main/java/com/jokosupriyanto/filmapp/ui/main/ViewModel.kt
@@ -0,0 +1,35 @@
+package com.jokosupriyanto.filmapp.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.jokosupriyanto.filmapp.data.model.Result
+import com.jokosupriyanto.filmapp.data.model.Show
+import com.jokosupriyanto.filmapp.data.repository.show.ShowRepository
+import com.jokosupriyanto.filmapp.data.repository.show.ShowRepositoryImpl
+
+class ViewModel: ViewModel() {
+    private val result = MutableLiveData<Show?>()
+    private val repository: ShowRepository = ShowRepositoryImpl()
+    public var error: String? = ""
+
+    fun fetchDetails() {
+        repository.findShow("naruto", object:ShowRepositoryImpl.Callback1 {
+            override fun success(show: Show?) {
+                result.postValue(show)
+            }
+
+            override fun failed(error: String?) {
+                this@ViewModel.error = error
+                result.postValue(null)
+            }
+        })
+
+    }
+
+    fun getDetail(): LiveData<Show?> {
+        return result
+    }
+
+
+}

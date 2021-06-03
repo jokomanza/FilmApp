4940de6f2faaec17c9d05c637e118cb82dd4fadb
Joko Supriyanto <jokomanza@gmail.com>
2021-06-03 15:22:44 +0700

    update show repository

--- app/src/main/java/com/jokosupriyanto/filmapp/data/repository/show/ShowRepository.kt
+++ app/src/main/java/com/jokosupriyanto/filmapp/data/repository/show/ShowRepository.kt
@@ -2,5 +2,5 @@ package com.jokosupriyanto.filmapp.data.repository.show
 
 interface ShowRepository {
 
-    fun findShow(key:String)
+    fun findShow(key:String, callback: ShowRepositoryImpl.Callback1)
 }
